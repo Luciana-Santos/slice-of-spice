3743473c9f9c75c8f39e153cb850baceb3441f82
Luciana Santos <luciydss@hotmail.com>
2022-12-02 22:10:46 -0300

    conteudo do menu adicionado

---
+++ src/content.js
@@ -0,0 +1,34 @@
+export const content = {
+  menuList: [
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Home',
+      url: 'index.html',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'About Us',
+      url: '#about',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Options',
+      url: '#options',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Menu',
+      url: '#menu',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Our Food',
+      url: '#ourFood',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Reservations',
+      url: '#reservations',
+    },
+  ],
+};
